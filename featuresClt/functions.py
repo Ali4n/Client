7235505957c472bbaefb84292ac3f442a033d17c
N3o <ali4npy@gmail.com>
2014-07-05 17:14:47 +0200

    ajout de la politique de login & mdp

--- featuresClt/functions.py
+++ featuresClt/functions.py
@@ -6,10 +6,23 @@ import hashlib
 
 def getLoginOrPassword(choice):
     if choice == 0:
-        return input("Saisir votre login")
+        doCondition = True
+        while doCondition:
+            login = input("Saisir votre login")
+            controlLenght = len(login)
+            if controlLenght < 6:
+                print("Notre politique de mot de passe vous impose d'avoir un login plus grand que 6 caracteres")
+            else:
+                break
+        return login
     elif choice == 1:
-        return input("Saisir votre mot de passe")
-
+        doCondition = True
+        while doCondition:
+            password = input("Saisir votre mot de passe")
+            controlLenght = len(password)
+            if controlLenght < 6:
+                print("Notre politique de mot de passe vous impose d'avoir un mot de passe plus grand que 6 caracteres")
+        return password
 
 def randomLoginOrPassword(choice):
 
@@ -20,7 +33,13 @@ def randomLoginOrPassword(choice):
     if choice == 0:
         while check == 1:
             try:
-                length = int(input("Choisisez la taille de votre login: "))
+                doCondition = True
+                while doCondition:
+                    length = int(input("Choisisez la taille de votre login: "))
+                    if length < 6:
+                        print("Notre politique de login vous impose d'avoir un login plus grand que 6 caracteres")
+                    else:
+                        break
                 break
             except:
                 print("Entrer un nombre")
@@ -29,7 +48,13 @@ def randomLoginOrPassword(choice):
     elif choice == 1:
         while check == 1:
             try:
-                length = int(input("Choisisez la taille de votre login: "))
+                doCondition = True
+                while doCondition:
+                    length = int(input("Choisisez la taille de votre mot de passe: "))
+                    if length < 6:
+                        print("Notre politique de mot de passe vous impose d'avoir un login plus grand que 6 caracteres")
+                    else:
+                        break
                 break
             except:
                 print("Entrer un nombre")
